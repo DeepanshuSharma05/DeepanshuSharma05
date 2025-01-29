018089c6e888002dd3161363d7ef5bbdf72a5571
DeepanshuSharma05 <sdeepanshu446@gmail.com>
2025-01-29 22:15:01 +0530

    Delete src directory

--- src/Index.js
+++
@@ -1,9 +0,0 @@
-// Custom JavaScript functionality can be added here
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('Custom JS file loaded.');
-
-    // Example of adding a click event to the "Post Requirement" button
-    document.querySelector('.nav-link').addEventListener('click', function() {
-        alert('Post Requirement button clicked!');
-    });
-});
